9c297fbf5419a62145e962efa06f12e3ed4997df
Swati <115008404+swatimry@users.noreply.github.com>
2025-01-02 12:11:57 +0530

    Update LoginAutomationTest.java

--- src/test/java/LoginAutomationTest.java
+++ src/test/java/LoginAutomationTest.java
@@ -1,34 +1,38 @@
 package com.example.automation;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-public class LoginAutomationTest {
- @Test
- public void testLogin() {
- // Set up the WebDriver
- System.setProperty("webdriver.chrome.driver", "C:\\Users\\ASUS\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
- WebDriver driver = new ChromeDriver();
- try {
- // Navigate to the login page
- driver.get("http://the-internet.herokuapp.com/login");
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
- // Locate the username and password fields
- WebElement usernameField = driver.findElement(By.id("username"));
- WebElement passwordField = driver.findElement(By.id("password"));
-  WebElement loginButton = driver.findElement(By.cssSelector("button[type='submit']"));
- // Perform login
- usernameField.sendKeys("tomsmith");
- passwordField.sendKeys("SuperSecretPassword!");
- loginButton.click();
- // Validate successful login
- WebElement successMessage = driver.findElement(By.cssSelector("div.flash.success"));
- assertTrue(successMessage.getText().contains("You logged into a secure area!"));
- } finally {
- // Close the browser
- driver.quit();
- }
- }
+public class LoginAutomationTest {
+    @Test
+    public void testLogin() {
+        // Set up the WebDriver
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\ASUS\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        try {
+            // Navigate to the login page
+            driver.get("http://the-internet.herokuapp.com/login");
+
+            // Locate the username and password fields
+            WebElement usernameField = driver.findElement(By.id("username"));
+            WebElement passwordField = driver.findElement(By.id("password"));
+            WebElement loginButton = driver.findElement(By.cssSelector("button[type='submit']"));
+
+            // Perform login
+            usernameField.sendKeys("tomsmith");
+            passwordField.sendKeys("SuperSecretPassword!");
+            loginButton.click();
+
+            // Validate successful login by checking for a success message
+            WebElement successMessage = driver.findElement(By.cssSelector("div.flash.success"));
+            assertTrue(successMessage.getText().contains("You logged into a secure area!"), "Login success message not found");
+        } finally {
+            // Close the browser
+            driver.quit();
+        }
+    }
 }
